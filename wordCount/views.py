ad3e41e73b8ad9d3db6b441fc0aba123f6b12d66
Astha Awasthi <asthaaw04@gmail.com>
2019-10-26 22:18:59 +0530

    added nav bar

--- wordCount/views.py
+++ wordCount/views.py
@@ -7,4 +7,15 @@ def home(request):
 def count(request):
     y=request.GET['text']
     l=y.split()
-    return render(request, 'count.html', {'xyz': len(l)})
+
+    dict={}
+    for word in l:
+        if word in dict:
+            dict[word]+=1
+        else:
+            dict[word]=1
+    print(dict)
+    return render(request, 'count.html' , { 'text': y , 'xyz': len(l) , 'dic': dict})
+
+def about(request):
+    return render(request,'about.html')
